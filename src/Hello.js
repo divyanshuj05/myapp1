227a910856897e3bf5c0cd76f1ed6edabef20219
divyanshuj05 <djain60_be20@thapar.edu>
2022-06-11 19:06:12 +0530

    1st

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return ( 
+        <div><h1>Hello world</h1>
+        <p>Welcome to React</p>
+        </div>
+        );
+    }
+}
+export default Hello;
